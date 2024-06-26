845b9d70c8bafc66c478f5475ad76e491f527d59
RFinity-solutions <163975602+RFinity-solutions@users.noreply.github.com>
2024-06-26 14:04:54 +0530

    Modified for using Pytest

--- drftemplate/drftemplate/settings/base.py
+++ drftemplate/drftemplate/settings/base.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 
-#Set DEBUG = False  For production
+#Set DEBUG = False  For production and True for development
 DEBUG = False
 
 
